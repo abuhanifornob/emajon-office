11e9f4c19c8eb0d09f4d4bf213709ea926b72c0f
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-09-27 17:05:34 +0600

    Hedding dd

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,7 @@ const Cart = (props) => {
         <div>
               <p className='order-text'>Order Summary</p>
                 <p>Selected Items:{props.cart.length}</p>
+                <h1>Heloo</h1>
         </div>
     );
 };
